3341e5be6b215be2e9ea7097a74f9791effa4c50
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-09 01:01:39 +0100

    .

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateArea(props) {
+ let row = 1;
+ const [isExpanded, setIsExpanded] = useState(false);
+
+ function expand() {
+  setIsExpanded(true);
+  row = 3;
+ }
  return (
   <div>
    <form>
-    <input
-     onChange={props.handleChange}
-     name='title'
-     placeholder='Title'
-     value={props.title}
-    />
+    {isExpanded == true ? (
+     <input
+      onChange={props.handleChange}
+      name='title'
+      placeholder='Title'
+      value={props.title}
+     />
+    ) : null}
     <textarea
+     onClick={expand}
      onChange={props.handleChange}
      name='content'
      placeholder='Take a note...'
-     rows='3'
+     rows={row}
      value={props.content}
     />
     <button onClick={props.addNote}>Add</button>
